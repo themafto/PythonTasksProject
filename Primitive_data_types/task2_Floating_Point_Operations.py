44dc15b21f0abc3b3b131387cc47d91fe1114d55
Artem <125770986+themafto@users.noreply.github.com>
2025-01-31 16:54:51 +0100

    Update Primitive_data_types/task2_Floating_Point_Operations.py
    Co-authored-by: Vadym Volkov <72186851+Wadosmp4@users.noreply.github.com>

--- Primitive_data_types/task2_Floating_Point_Operations.py
+++ Primitive_data_types/task2_Floating_Point_Operations.py
@@ -1,4 +1,4 @@
 radius = float(input("Write radius to calculate the area of a circle:\n"))
 p = 3.14159
-area = p * radius**2
-print("Area of a circle = " + str(area))
+area = p * radius ** 2
+print(f"Area of a circle = {area}")
