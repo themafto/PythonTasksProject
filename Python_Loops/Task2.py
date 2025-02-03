aa1ba806bd4a8fe01b85e28afbf9f4a1a71cb930
Artem <125770986+themafto@users.noreply.github.com>
2025-02-03 16:50:50 +0100

    Update Python_Loops/Task2.py
    Co-authored-by: Vadym Volkov <72186851+Wadosmp4@users.noreply.github.com>

--- Python_Loops/Task2.py
+++ Python_Loops/Task2.py
@@ -1,7 +1,7 @@
 list_of_integer = [10, 20, 30, 40, 50, 11, 17, 22]
 summ = 0
 count = 0
-for i,value in enumerate(list_of_integer):
+for i, value in enumerate(list_of_integer):
     if value % 2 == 0:
         summ += value
         count += 1
