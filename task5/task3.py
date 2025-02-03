f397c83443fac915106f79d8655e1f242d5b808a
Artem <125770986+themafto@users.noreply.github.com>
2025-02-03 18:44:04 +0100

    Improved
    Co-authored-by: Vadym Volkov <72186851+Wadosmp4@users.noreply.github.com>

--- task5/task3.py
+++ task5/task3.py
@@ -2,5 +2,5 @@ books = {
     "book1": {"title": "1984", "author": "George Orwell", "year": 1949},
     "book2": {"title": "To Kill a Mockingbird", "author": "Harper Lee", "year": 1960}
 }
-for key,value in books.items():
+for value in books.values():
     print(f'Book: {value["title"]}, Author:  {value["author"]}, Year: {value["year"]}')
