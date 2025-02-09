14fb65ce2364dbbe0a8dbce229a966f68e567387
Artem <125770986+themafto@users.noreply.github.com>
2025-02-09 13:36:00 +0100

    Update OOP_python/task3.py
    Co-authored-by: Vadym Volkov <72186851+Wadosmp4@users.noreply.github.com>

--- OOP_python/task3.py
+++ OOP_python/task3.py
@@ -2,8 +2,10 @@ class Rectangle:
     def __init__(self, width, height):
         self.width = width
         self.height = height
+        
     def area(self):
         return self.width * self.height
+
 
 rectangle1 = Rectangle(5, 10)
 print(rectangle1.width, rectangle1.height)
