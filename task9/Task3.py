0752bc72ef152b08e43b2b0c2969d93d6af3d150
Artem <125770986+themafto@users.noreply.github.com>
2025-02-09 14:28:24 +0100

    improvement
    Co-authored-by: Vadym Volkov <72186851+Wadosmp4@users.noreply.github.com>

--- task9/Task3.py
+++ task9/Task3.py
@@ -5,7 +5,7 @@ from datetime import datetime
 file_name = "log.txt"
 file_exists = os.path.exists(file_name)
 
-with open("log.txt", mode = "a") as log:
+with open(file_name, mode = "a") as log:
     current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     log.write(f"\nCurrent Date and Time: {current_time}\n")
 
