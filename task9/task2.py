08da8281080187fad4f8b71913e6925293079f8b
Artem <125770986+themafto@users.noreply.github.com>
2025-02-09 14:29:00 +0100

    fixed --> using same path to file
    Co-authored-by: Vadym Volkov <72186851+Wadosmp4@users.noreply.github.com>

--- task9/task2.py
+++ task9/task2.py
@@ -1,7 +1,9 @@
-with open("sample.txt", mode = "w") as file:
+FILE_PATH = "sample.txt"
+
+with open(FILE_PATH, mode = "w") as file:
     file.write("Hello world!")
 
-with open("sample.txt", mode = "r") as file:
+with open(FILE_PATH, mode = "r") as file:
     content = file.read()
     word_count = len(content.split())
     print(content, "\nWord count:", word_count)
